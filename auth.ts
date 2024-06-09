af1aa7c95158fa55a58b0182b1b74ae74a39f2f8
Laksito Pamilih <laksito@pm.me>
2024-06-09 15:43:38 +0700

    remove auth custom error

--- auth.ts
+++ auth.ts
@@ -38,6 +38,8 @@ export const { auth, signIn, signOut } = NextAuth({
         if (passwordsMatch) return user;
       }
 
+      console.log('Invalid credentials');
+      return null;
       throw new CustomError('Invalid Credentials')
     },
   })],
